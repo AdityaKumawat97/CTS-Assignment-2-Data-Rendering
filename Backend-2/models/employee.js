ef80faa0a36deae726396230392b0f27750c9d3f
Aditya_kumawat <31047132+AdityaKumawat97@users.noreply.github.com>
2020-07-09 23:08:53 +0530

    Added Edit/save btuttons , made the edit button fully functional

--- Backend-2/models/employee.js
+++ Backend-2/models/employee.js
@@ -5,6 +5,11 @@ const EmployeeSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    ID: {
+        type: String,
+        required: true,
+        default: "123456"
+    },
     skills: {
         type: Array,
         required: true
@@ -12,7 +17,17 @@ const EmployeeSchema = new mongoose.Schema({
     gender: {
         type: String,
         required: true
-    }
+    },
+    Project: {
+        type: String,
+        required: true,
+        default: "Cognizant Internal"
+    },
+    HCM: {
+        type: String,
+        required: true,
+        default: "Alex"
+    },
 })
 
 module.exports = mongoose.model('Employees', EmployeeSchema)
